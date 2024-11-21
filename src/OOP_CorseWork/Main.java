bd427845fb24e28b6d4fc316eb4664e9a8397d88
Tharanafernando <tharana.611@gmail.com>
2024-11-21 22:46:05 +0530

    add Main class

--- src/OOP_CorseWork/Main.java
+++ src/OOP_CorseWork/Main.java
@@ -4,13 +4,26 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input Max Ticket Capacity:");
+        int max = sc.nextInt();
+
+        System.out.println("Input Total Number of Tickets:");
+        int total = sc.nextInt();
+
+        System.out.println("Input Ticket Release Rate: ");
+        double rate = sc.nextDouble();
+
+        System.out.println("Input Customer Retrival Rate: ");
+        double customerRate = sc.nextDouble();
 
 
-
-
-
+        Configuration configuration = new Configuration(max, total, rate, customerRate);
     }
 
 
